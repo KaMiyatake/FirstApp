8c3ac5b06f6371d0e79811f205ee180e1867bc5e
KaMiyatake <come_on_myself@hotmail.com>
2025-07-08 10:48:44 +0900

    feat さらにテスト修正

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>こんにちは！こちらはテストアプリです。</Text>
+      <Text>こんにちは！こちらはテストアプリです。さらに追加！</Text>
       <StatusBar style="auto" />
     </View>
   );
